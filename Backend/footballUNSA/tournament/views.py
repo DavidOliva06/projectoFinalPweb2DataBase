298ff8c31ed26541f56b5e00829e148861196445
David Oliva <doliva@unsa.edu.pe>
2025-07-06 11:42:13 -0500

    Views.py

--- Backend/footballUNSA/tournament/views.py
+++ Backend/footballUNSA/tournament/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Faculty, Team, Player, Fixture
+from .serializers import FacultySerializer, TeamSerializer, PlayerSerializer, FixtureSerializer
 
-# Create your views here.
+class FacultyViewSet(viewsets.ModelViewSet):
+    queryset = Faculty.objects.all()
+    serializer_class = FacultySerializer
+
+class TeamViewSet(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+class PlayerViewSet(viewsets.ModelViewSet):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+
+class FixtureViewSet(viewsets.ModelViewSet):
+    queryset = Fixture.objects.all()
+    serializer_class = FixtureSerializer
+
