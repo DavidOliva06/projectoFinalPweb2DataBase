24e5c29fc256dbd7ac29cd550dece99c59f52fcb
David Oliva <doliva@unsa.edu.pe>
2025-07-06 17:30:29 -0500

    Modelos.png

--- Backend/footballUNSA/tournament/admin.py
+++ Backend/footballUNSA/tournament/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Faculty, Team, Player, Fixture
 
-# Register your models here.
+admin.site.register(Faculty)
+admin.site.register(Team)
+admin.site.register(Player)
+admin.site.register(Fixture)
