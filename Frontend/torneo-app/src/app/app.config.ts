0bb6e8d4dac1d859bf093e866a031e0dc9da5d04
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-07-14 17:00:00 -0500

    feat(api): Implementa HttpInterceptor para adjuntar tokens JWT automáticamente

--- Frontend/torneo-app/src/app/app.config.ts
+++ Frontend/torneo-app/src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http'; 
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { jwtInterceptor } from './services/jwt-interceptor';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
@@ -11,6 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes), provideClientHydration(withEventReplay()),
-    provideHttpClient()
+    provideAnimations(),
+    provideHttpClient(withInterceptors([jwtInterceptor]))
   ]
 };
