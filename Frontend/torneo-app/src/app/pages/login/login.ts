bf02d9c0236e92f0f0ec36c41b108e22a4e8e16d
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-07-17 10:00:00 -0500

    feat(fixture): Crea componente y servicio para obtener los datos del fixture

--- Frontend/torneo-app/src/app/pages/login/login.ts
+++ Frontend/torneo-app/src/app/pages/login/login.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router'; 
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
 
+  credentials = {
+    email: '',
+    password: ''
+  };
+
+  isLoading = false;
+  errorMessage: string | null = null;
+
+  constructor(
+    private authService: Auth,
+    private router: Router
+  ) {}
+
+  onSubmit(): void {
+    this.isLoading = true;
+    this.errorMessage = null;
+
+    this.authService.login(this.credentials).subscribe({
+      next: () => {
+        console.log('Login exitoso!');
+        this.router.navigate(['/fixture']);
+      },
+      error: (err) => {
+        this.isLoading = false;
+        this.errorMessage = 'Credenciales inválidas. Por favor, inténtalo de nuevo.';
+        console.error('Error en el login:', err);
+      }
+    });
+  }
 }
