50781ad0042458189f8b5a4c0b2431bd33f39d79
Edson Fabricio Subia Huaicane <esubiahu@unsa.edu.pe>
2025-07-17 10:00:00 -0500

    feat(fixture): Crea componente y servicio para obtener los datos del fixture

--- Frontend/torneo-app/src/app/app.routes.ts
+++ Frontend/torneo-app/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { MainLayout } from './layouts/main-layout/main-layout';
 import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
+import { FixtureList } from './pages/fixture-list/fixture-list';
 
 export const routes: Routes = [
   // Todas las rutas principales usarán nuestro MainLayoutComponent como base
@@ -16,8 +17,7 @@ export const routes: Routes = [
 
       { path: 'registro', component: Register, title: 'Registro - TorneoPro' },
 
-      // Dejamos preparada la ruta para el fixture que haremos más adelante
-      // { path: 'fixture', component: FixtureListComponent, title: 'Fixture - TorneoPro' },
+      { path: 'fixture', component: FixtureList, title: 'Fixture - TorneoPro' },
     ]
   },
   // { path: '**', component: NotFoundComponent }
